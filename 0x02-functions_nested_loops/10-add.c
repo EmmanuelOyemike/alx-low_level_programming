2d23f424ecccb588fc8dc7de2fba0aee67c04ede
Iscah <emmaoyemike@gmail.com>
2022-04-12 12:18:46 -0700

    work on C

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,22 +1,11 @@
 #include "main.h"
-#include <stdio.h>
 /**
-* print_to_98 - print to 98
-* @n : number to start from
-* Return:0 or 1
+* add - returns the sum of its parameters
+* @a: int type number
+* @b: int type number
+* Return:0
 */
-void print_to_98(int n)
+int add(int a, int b)
 {
-while (n < 98)
-{
-printf("%i, ", n);
-n++;
-}
-while (n > 98)
-{
-printf("%i, ", n);
-n--;
-}
-printf("98");
-putchar('\n');
+return (a + b);
 }
