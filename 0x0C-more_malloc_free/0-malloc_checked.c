d725eb1750bd1a0b054b64563ec26760895bf4ec
Iscah <emmaoyemike@gmail.com>
2022-05-10 06:02:35 -0700

    more malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+
 /**
  * malloc_checked - allocates memory.
  * @b: amount of bytes.
@@ -7,7 +8,8 @@
  * Return: pointer to the allocated memory.
  * if malloc fails, status value is equal to 98.
  */
-void *malloc_checked(unsigned int b)
+
+	void *malloc_checked(unsigned int b)
 {
 	char *p;
 
